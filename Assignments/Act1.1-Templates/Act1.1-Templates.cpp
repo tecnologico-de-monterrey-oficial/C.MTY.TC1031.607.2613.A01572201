a103335d39b85ed7fca9da3fd3361530aaf32c5f
Daniel-G-G <a01572201@tec.mx>
2026-08-14 16:53:08 -0600

    Class02-avanceAct1

---
+++ Assignments/Act1.1-Templates/Act1.1-Templates.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#include "List.h"
+
+/**
+int sum(int a, int b) {
+    return a + b;
+}
+
+double sum(double a, double b) {
+    return a + b;
+}
+
+string sum(string a, string b) {
+    return a + b;
+}
+**/
+
+template <typename T>
+T sum(T a, T b) {
+    return a + b;
+}
+
+int main() {
+
+    string a = "hola ";
+    string b = "crayola";
+    cout << "Sum of " << a << " and " << b << " is: " << sum(a, b) << endl;
+    
+    int c = 5;
+    int d = 10;
+    cout << "Sum of " << c << " and " << d << " is: " << sum(c, d) << endl;
+
+    double e = 5.5;
+    double f = 10.5;
+    cout << "Sum of " << e << " and " << f << " is: " << sum(e, f) << endl;
+
+    List<int> list; // llama al constructor default
+    list.insert(5);
+    list.insert(10);
+    list.insert(15);
+
+    List<string> things;
+    things.insert("Laptop");
+    things.insert("bottle");
+
+    return 0;
+}
+
---
+++ Assignments/Act1.1-Templates/List.h
@@ -0,0 +1,30 @@
+#ifndef List_h
+#define List_h
+
+#include <vector>
+
+template <typename T>
+class List {
+    private:
+        vector<T> list;
+        int size;
+    public:
+        List();
+        void insert(T data);
+};
+
+template <typename T>
+List<T>::List() {
+    size = 0;
+}
+
+template <typename T>
+void List<T>::insert(T data) {
+    list.push_back(data);
+    size++;
+}
+
+
+
+
+#endif /* List_h */
