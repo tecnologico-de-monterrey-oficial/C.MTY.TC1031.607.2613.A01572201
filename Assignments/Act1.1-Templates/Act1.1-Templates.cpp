d571a41d7dacb22510dc4ab0c0ffd360652f9ee2
Daniel-G-G <a01572201@tec.mx>
2026-08-16 21:22:02 -0600

    Avance

--- Assignments/Act1.1-Templates/Act1.1-Templates.cpp
+++ Assignments/Act1.1-Templates/Act1.1-Templates.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 #include "List.h"
 
 /**
--- Assignments/Act1.1-Templates/List.h
+++ Assignments/Act1.1-Templates/List.h
@@ -1,7 +1,9 @@
 #ifndef List_h
 #define List_h
 
+#include <iostream>
 #include <vector>
+using namespace std;
 
 template <typename T>
 class List {
@@ -11,6 +13,13 @@ class List {
     public:
         List();
         void insert(T data);
+        void removeLast();
+        T getData(int position);
+        int getSize();
+        T getMax();
+        void print();
+        void insertAt(int position, T data);
+        void removeAt(int position);
 };
 
 template <typename T>
@@ -24,7 +33,78 @@ void List<T>::insert(T data) {
     size++;
 }
 
+template <typename T>
+void List<T>::removeLast() {
+    if (size == 0) {
+        cout << "NO HAY ELEMENTOS" << endl;
+    } else {
+        cout << list[size - 1] << endl;
+        list.pop_back();
+        size--;
+    }
+}
 
+template <typename T>
+T List<T>::getData(int position) {
+    if (0 <= position && position < size) {
+        return list[position];
+    } else {
+        cout << "POSICIÓN INVÁLIDA" << endl;
+        return T();
+    }
+}
 
+template <typename T>
+int List<T>::getSize() {
+    return size;
+}
+
+template <typename T>
+T List<T>::getMax() {
+    if (size == 0) {
+        cout << "NO HAY ELEMENTOS" << endl;
+        return T();
+    } else {
+        T maxValue = list[0];
+
+        for (int i = 1; i < size; i++) {
+            if (list[i] > maxValue) {
+                maxValue = list[i];
+            }
+        }
+
+        return maxValue;
+    }
+}
+
+template <typename T>
+void List<T>::print() {
+    for (int i = 0; i < size; i++) {
+        cout << "[" << i << "] - " << list[i] << endl;
+    }
+}
+
+template <typename T>
+void List<T>::insertAt(int position, T data) {
+    if (0 <= position && position <= size) {
+        list.insert(list.begin() + position, data);
+        size++;
+    } else {
+        cout << "POSICIÓN INVÁLIDA" << endl;
+    }
+}
+
+template <typename T>
+void List<T>::removeAt(int position) {
+    if (0 <= position && position < size) {
+        cout << list[position] << endl;
+        list.erase(list.begin() + position);
+        size--;
+    } else if (size == 0) {
+        cout << "NO HAY ELEMENTOS" << endl;
+    } else {
+        cout << "POSICIÓN INVÁLIDA" << endl;
+    }
+}
 
 #endif /* List_h */
