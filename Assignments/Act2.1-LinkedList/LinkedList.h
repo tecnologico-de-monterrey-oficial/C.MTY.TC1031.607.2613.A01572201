6a4f0b4ef9902b90ac9363a2e53d593d2fc848b7
Daniel-G-G <a01572201@tec.mx>
2026-09-25 15:27:59 -0600

    smallchange

--- Assignments/Act2.1-LinkedList/LinkedList.h
+++ Assignments/Act2.1-LinkedList/LinkedList.h
@@ -9,7 +9,8 @@
 template <typename T>
 class LinkedList {
     private:
-    std::unique_ptr< Node<T> > head;
+    //std::unique_ptr< Node<T> > head;
+    Node<T>* head;
     int size;
     public:
     LinkedList() : head(nullptr), size(0) {}
@@ -21,21 +22,26 @@ class LinkedList {
 template <typename T>
 void LinkedList<T>::push_front(T data) {
     // crear un nodo nuevo
-    std::unique_ptr< Node<T> > node = std::make_unique< Node<T> >(data);
+    //std::unique_ptr< Node<T> > node = std::make_unique< Node<T> >(data);
+    Node<T>* node = new Node<T>(data);
     // actualizo el next del nodo nuevo para que apunte a head
-    node->next = std::move(head);
+    //node->next = std::move(head);
+    node->next = head;
     // actualizo head
-    head = std::move(node);
+    //head = std::move(node);
+    head = node;
 };
 
 template <typename T>
 void LinkedList<T>::print() {
     // creamos un apuntador auxiliar que apunte a head
-    Node<T>* aux = head.get();
+    //Node<T>* aux = head.get();
+    Node<T>* aux = head;
     // recorremos la lista mientras aux sea diferente de nullptr
     while (aux != nullptr) {
         std::cout << aux->data;
-        aux = aux->next.get();
+        //aux = aux->next.get();
+        aux = aux->next;
         if (aux != nullptr) {
             std::cout << "-";
         }
